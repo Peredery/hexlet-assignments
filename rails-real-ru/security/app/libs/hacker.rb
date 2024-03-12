d37718d99c88e14df85c623824983e193e4bd0d3
@hexlet/cli <support@hexlet.io>
2024-03-12 14:17:28 +0500

    submit rails-real-ru/security

--- rails-real-ru/security/app/libs/hacker.rb
+++ rails-real-ru/security/app/libs/hacker.rb
@@ -1,37 +1,54 @@
 # frozen_string_literal: true
 
 require 'open-uri'
+require 'nokogiri'
+require 'net/http'
+require 'net/https'
 
 class Hacker
   class << self
     def hack(email, password)
+      # BEGIN
       hostname = 'https://rails-collective-blog-ru.hexlet.app'
-      reg_path = '/users/sign_up/'
-      reg_post_path = '/users'
+      get_path = '/users/sign_up'
+      post_path = '/users'
 
       uri = URI(hostname)
       http = Net::HTTP.new(uri.host, uri.port)
-      response = Net::HTTP.get_response(URI.join(hostname, reg_path))
+      http.use_ssl = uri.scheme == 'https'
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      token = Nokogiri::HTML(response.body).at('input[@name="authenticity_token"]')['value']
-      cookies = response['set-cookie']
+      req = Net::HTTP::Get.new URI.join(hostname, get_path)
 
-      reg_req = Net::HTTP::Post.new(URI.join(hostname, reg_post_path))
-      reg_req['Cookie'] = cookies
-      reg_req['Content-Type'] = 'application/x-www-form-urlencoded'
-      reg_req.body = URI.encode_www_form(
-        'user[email]' => email,
-        'user[password]' => password,
-        'user[password_confirmation]' => password,
-        'authenticity_token' => token,
-        'commit' => 'Регистрация'
-      )
+      res = http.request req
 
-      reg_response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-        http.request(reg_req)
-      end
+      cookie = res.response['set-cookie'].split('; ')[0]
+      # csrf_token = html.at('meta[name="csrf-token"]')['content']
 
-      reg_response.code == '303'
+      params = {
+        'user[email]': email,
+        'user[password]': password,
+        'user[password_confirmation]': password,
+        authenticity_token: auth_token(res.body)
+      }
+
+      req = Net::HTTP::Post.new URI.join(hostname, post_path)
+      req.body = URI.encode_www_form(params)
+      req['Cookie'] = cookie
+      # req['X-Csrf-Token'] = csrf_token
+
+      res = http.request req
+
+      res.code == '302'
     end
+
+    private
+
+    def auth_token(body)
+      html = Nokogiri::HTML(body)
+
+      html.at('input[@name="authenticity_token"]')['value']
+    end
+    # END
   end
 end
