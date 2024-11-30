e297f7eab6fb1e3f6da39af138cf40a2ddb760ed
@hexlet/cli <support@hexlet.io>
2024-11-30 21:37:22 +0500

    submit rails-full-ru/testing-2

--- rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
+++ rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
@@ -4,6 +4,8 @@ require 'test_helper'
 
 class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
   # BEGIN
-
+  test 'create' do
+    expect(Repository.count).must_equal 1
+  end
   # END
 end
