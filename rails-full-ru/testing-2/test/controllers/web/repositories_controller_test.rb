22dfea6218171a8dba8932897b7dabb1e390ebfe
@hexlet/cli <support@hexlet.io>
2024-11-30 22:38:26 +0500

    submit rails-full-ru/testing-2

--- rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
+++ rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
@@ -2,27 +2,30 @@
 
 require 'test_helper'
 
-class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
-  # BEGIN
-  test 'should_create' do
-    stubbed_data = load_fixture('files/response.json')
+module Web
+  class RepositoriesControllerTest < ActionDispatch::IntegrationTest
+    # BEGIN
+    test 'should_create' do
+      stubbed_data = load_fixture('files/response.json')
 
-    repo_link = 'https://github.com/rails/rails'
+      repo_link = 'https://github.com/rails/rails'
 
-    stub_request(
-      :get, 'https://api.github.com/repos/rails/rails'
-    ).with(
-      headers: {
-        'Accept'=>'application/vnd.github.v3+json',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Content-Type'=>'application/json',
-        'User-Agent'=>'Octokit Ruby Gem 9.1.0'
-        }).
-    to_return(status: 200, body: stubbed_data,  headers: { 'Content-Type': 'application/json' })
+      stub_request(
+        :get, 'https://api.github.com/repos/rails/rails'
+      ).with(
+        headers: {
+          'Accept' => 'application/vnd.github.v3+json',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type' => 'application/json',
+          'User-Agent' => 'Octokit Ruby Gem 8.1.0'
+          }
+      )
+        .to_return(status: 200, body: stubbed_data, headers: { 'Content-Type': 'application/json' })
 
-    post repositories_path, params: { repository: { link: repo_link } }
+      post repositories_path, params: { repository: { link: repo_link } }
 
-    assert_response :redirect
+      assert_response :redirect
+    end
+    # END
   end
-  # END
 end
