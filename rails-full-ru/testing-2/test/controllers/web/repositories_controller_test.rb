13cb135df31ffad569615809a6b60bf981b498ad
@hexlet/cli <support@hexlet.io>
2024-11-30 22:30:42 +0500

    submit rails-full-ru/testing-2

--- rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
+++ rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
@@ -4,8 +4,25 @@ require 'test_helper'
 
 class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
   # BEGIN
-  test 'create' do
-    expect(Repository.count).must_equal 1
+  test 'should_create' do
+    stubbed_data = load_fixture('files/response.json')
+
+    repo_link = 'https://github.com/rails/rails'
+
+    stub_request(
+      :get, 'https://api.github.com/repos/rails/rails'
+    ).with(
+      headers: {
+        'Accept'=>'application/vnd.github.v3+json',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Content-Type'=>'application/json',
+        'User-Agent'=>'Octokit Ruby Gem 9.1.0'
+        }).
+    to_return(status: 200, body: stubbed_data,  headers: { 'Content-Type': 'application/json' })
+
+    post repositories_path, params: { repository: { link: repo_link } }
+
+    assert_response :redirect
   end
   # END
 end
